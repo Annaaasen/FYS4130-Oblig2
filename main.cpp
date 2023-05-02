bab1d0d34476d9b752bd75314c26f9b08775c1f9
Annaaasen <65226318+Annaaasen@users.noreply.github.com>
2023-05-02 14:00:10 +0200

    Added a lot of comments regarding my understanding. Made it seemingly work for 2D.

--- main.cpp
+++ main.cpp
@@ -7,51 +7,64 @@
 using namespace std;
 #define PI 3.141592653589793238462643
 
+int power(int base, int exp) {
+    int result = 1;
+    for (int i = 0; i < exp; i++) {
+        result *= base;
+    }
+    return result;
+}
+
 const int q = 3;                                // q spin states
 const int L = 16;                               // linear system size 
 const double T = 0.5;
 
-const int N = L;                                // Total number of spins 
+const int NDIMS = 2;                            // Number of dimensions 
+const int N = power(L, NDIMS);                                // Total number of spins 
 const double pconnect = 1 - exp(-1.0/T);        // connection probability 
 
 const int NCLUSTERS = 1;
 const int NESTEPS = 100000;
 const int NMSTEPS = 100000;
-const int NBINS = 10;                           // # measurement bins (?)
+const int NBINS = 10;                           // # measurement bins (# experiments)
 
 vector<int> S(N);                               // spin array 
 vector<int> M(q);                               // # spins in different states 
-vector <complex<double> > W(q);                 // order parameter weights (?)
+vector <complex<double> > W(q);                 // returns mj-value given a certain state (s)
 
 // lattice handling: 
-enum dirs{RIGHT,LEFT};
-int indx(int x){return x;}
-// int indx(int x, int y){ return y * L + x; }                    // Make an index on every site 
+enum dirs{RIGHT,LEFT, UP, DOWN};
+// int indx(int x){return x;}
+int indx(int x, int y){ return y * L + x; }                    // Make an index on every site 
 int xpos(int i){ return i%L;}
-// int ypos(int i){ return ;}
+int ypos(int i){ return i/L;}
 
 int Nbr(int i, int dir) {
+    // i is the index of the point for which we want to find the neighbouring indeces
+    // dir is in which direction we want to find the neighbouring index (is an integer which refers to the 'list' dirs)
     int x=xpos(i);
+    int y=ypos(i);
     switch (dir)
     {
-        case RIGHT: 
-            return indx((x+1)%L);
-        case LEFT: 
-            return indx((x-1+L)%L);
+        case RIGHT: return indx((x+1)%L, y);
+        case LEFT: return indx((x-1+L)%L, y);
+        case UP: return indx(x, ((y+1)%L));
+        case DOWN: return indx(x,(y-1+L)%L);
     }
     return 0;
 }
 
 
 void FlipandBuildFrom(int s) {
-    int oldstate(S[s]), newstate((S[s]+1)%q);
+    // In this case s refers to the site (I think), a random site is often sent in 
+    int oldstate(S[s]), newstate((S[s]+1)%q); //asigning the value of S[s] to oldstate, the newstate will be one state 'larger'
 
     S[s] = newstate;
-    M[oldstate]--; M[newstate]++;               // Update spin counts
+    M[oldstate]--; M[newstate]++;                       // Update spin counts
 
-    for(int dir = 0; dir < 2; dir++) {          // Go through neighbours
-        int j = Nbr(s, dir);
-        if(S[j] == oldstate)
+    for(int dir = 0; dir < NDIMS*2; dir++) {            // Go through neighbours
+        int j = Nbr(s, dir);                            // How can j become something?? Nbr returns 0, but only if dir is out of reach of dirs (eg dir=4)
+        if(S[j] == oldstate)                            // If the neighbour has the same state as before the flip (shouldn't this also happen for the third possible state all that matters is that they are in different states, right?)
             if( rand()/(RAND_MAX+1.) < pconnect)
                 FlipandBuildFrom(j); 
     }
@@ -65,26 +78,26 @@ int main()
         W[s] = complex<double>(cos(2*PI*s/q), sin(2*PI*s/q)); //Gives the mj-value
     for(int i = 0; i < N; i++) S[i] = 0;
     for(int s = 1; s < q; s++) M[s] = 0;
-    M[0] = N;
+    M[0] = N;                                                 //All particles start in state 0
     srand((unsigned) time(0));
 
     // equilibriate 
     for(int t = 0; t < NESTEPS; t++) {
         for(int c = 0; c < NCLUSTERS; c++) {
-            FlipandBuildFrom(rand()%N);
+            FlipandBuildFrom(rand()%N);                     //rand()%N Make sure a number larger than N-1 is never sent in 
         }
     }
 
     // measure 
     for(int n = 0; n < NBINS; n++) {
-        complex<double> m(0., 0.);
+        complex<double> m(0., 0.);                          // Initialize (start with no magnetization)
         double m1 = 0., m2 = 0., m4 = 0.;
 
         for(int t = 0; t < NMSTEPS; t++) {
-            for(int c = 0; c < NCLUSTERS; c++)
-                FlipandBuildFrom(rand()%N);
+            for(int c = 0; c < NCLUSTERS; c++)              //If NCLUSTERS>1, there are more than one cluster, and this will be start several flipandbuildfrom
+                FlipandBuildFrom(rand()%N);                 //Determines all of the states in the grid after the Wolff cluser algo
 
-            complex<double> tm(0., 0.);
+            complex<double> tm(0., 0.);                     //'counts' the magnetizations given the specific grid 
             for(int s = 0; s < q; s++)
                 tm += W[s]*double(M[s]);
 
---
+++ plotting.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+
+#Setting T = T/J and k_B=1
+
+N = 16
+Ts = np.array([0.25, 0.5])
+r = np.linspace(0, 16, 100)
+
+#####################
+### Analytic part ###
+####################
+def analytic_C(N, r, T):
+    beta = 1/T
+    a = np.exp(beta) + 2
+    b = np.exp(beta) - 1
+    nom = a**r * b**(N-r) + b**r * a**(N-r) + b**N
+    denom = 2*b**N + a**N
+    return nom / denom 
+
+for T in Ts:
+    plt.plot(r, analytic_C(N, r, T))
+plt.show()
+
+
+
